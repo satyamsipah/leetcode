d218ce30048f2ea7fd225cb9fbcab8c3e883e4cb
Satyam Maddheshiya <127894454+satyamsipah@users.noreply.github.com>
2025-02-01 15:58:27 +0530

    Create search_a_2D_matrix.cpp

---
+++ search_a_2D_matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    
+    int i = n - 1, j = 0;
+
+    while(i >= 0 && j < m) {
+        if(matrix[i][j] == target) {
+            cout << "found your cell at (" << i << "," << j << ")" << endl;
+            return true;
+        }
+        else if(matrix[i][j] < target) {
+            j++;
+        }
+        else {
+            i--;
+        }
+    }
+    cout << "element is not found" << endl;
+    return false;
+
+    }
+};
