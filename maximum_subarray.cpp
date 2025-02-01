6fdc327063b86a458c6051ba1add2f7cbc27b33b
Satyam Maddheshiya <127894454+satyamsipah@users.noreply.github.com>
2025-02-02 01:34:51 +0530

    Create maximum_subarray.cpp

---
+++ maximum_subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+    int n = nums.size();
+
+    int maxSum = INT_MIN;
+    int currSum = 0;
+
+    for(int i=0; i<n; i++) {
+        currSum += nums[i];
+        maxSum = max(maxSum , currSum);
+        if(currSum < 0) {
+            currSum = 0;
+        }
+    }
+    return maxSum;
+    }
+};
